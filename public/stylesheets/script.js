b79bd0af1766b59db40f8d9772be70c05708858b
nguyen-thi-mai01 <165255175+nguyen-thi-mai01@users.noreply.github.com>
2025-03-04 22:06:07 +0700

    Update script.js

--- public/stylesheets/script.js
+++ public/stylesheets/script.js
@@ -9,10 +9,10 @@ function showSlides() {
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
-  for (i = 0; i < slides.length; i++) { // Thêm vòng lặp này để loại bỏ class 'fade'
+  for (i = 0; i < slides.length; i++) { 
     slides[i].classList.remove('fade');
   }
   slides[slideIndex-1].style.display = "block";
-  slides[slideIndex-1].classList.add('fade'); // Thêm lại class fade cho slide hiện tại
-  setTimeout(showSlides, 5000); // Chuyển slide sau 5 giây
+  slides[slideIndex-1].classList.add('fade'); 
+  setTimeout(showSlides, 5000); 
 }
